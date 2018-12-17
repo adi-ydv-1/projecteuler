0ee52c8ac844e59e88bbece69422d24bde4b893a
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2018-12-17 22:35:39 +0530

    Update EULAR.java

--- EULAR.java
+++ EULAR.java
@@ -1,14 +1,11 @@
 
 package q1;
-
-import java.util.Scanner;
 public class EULAR {
 
    
     public static void main(String[] args) 
     {
         int i,sum=0;
-        Scanner sc=new Scanner(System.in);
         for(i=1;i<1000;i++)
         {
             if(i%3==0||i%5==0)
