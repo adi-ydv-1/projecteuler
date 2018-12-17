5b5ad520283a93e34473c3c6ca3d930b0710ba68
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2018-12-17 22:20:54 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,14 +1,12 @@
 
 package Q7;
 
-import java.util.Scanner;
 public class prime
 {
     public static void main(String args[])
     {
         int i,j=2,k=0,m=0;boolean f=false;
-        Scanner sc=new Scanner(System.in);
-        for(i=2;i<123456788;i++)
+        for(i=2; ;i++)
         {
             while(j<i)
             {
@@ -28,20 +26,11 @@ public class prime
                    {
                       m=i;
                    break;
-                   
-                   
                    }
                    
                 }
                f=false;
         }
-        System.out.println("10001 prime no is "+m);
-    
-    
-    
-    
-    
-    
-    
+        System.out.println("10001 prime no is "+m);    
 }
 }
