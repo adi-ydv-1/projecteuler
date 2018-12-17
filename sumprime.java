4aa682af64fc661c9c5570f2003ea3324c3410a8
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2018-12-17 22:45:25 +0530

    Update sumprime.java

--- sumprime.java
+++ sumprime.java
@@ -1,12 +1,10 @@
 
 package Q10
-import java.util.Scanner;
 public class sumprime
 {
     public static void main(String args[])
     {
         long i,sum=0,k=2l;boolean f=false;
-        Scanner sc=new Scanner(System.in);
         for(i=2;i<2000000L;i++)
         {
             while(k<i)
