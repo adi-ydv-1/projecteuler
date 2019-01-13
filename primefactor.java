69e406888f5addf7e299d105270f723776af432e
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2019-01-13 20:17:05 +0530

    Update primefactor.java

--- primefactor.java
+++ primefactor.java
@@ -1,55 +1,30 @@
 
-package Q3;
-        
+package Q3;       
 public class primefactor 
 {
-    public static void main(String args[])
-    {
-        
-        
-        long max=0;
-        long i;long j=2l,x=0;boolean f=false;
-        int c=0;   
-        
-        
-        for(i=2;i<600851475143l;i++)
-        {
-            
-                if(i>123456780)
-                    break;
-            if(600851475143l%i==0l)         
-            {
-                while(j<i)
-                {
-                    if(i%j==0)
-                    {
-                    f=true;
-                    
-                    break;
-                    }
-                    j++;          
-          
-                if(f==false&&i>max)
-                {
-                     
-                    max=i;
-                    
-                                       
-                } 
-            }
-            j=2;
-            f=false;
-        }
-         
-          
-            System.out.println("Largest prime factor of "  +max);
-        
-            
-    
-    
-    
-    
-    
-    
-}
+ public static void main(String args[])
+	    {	        
+	        long i;long j=2,x=0;
+	             
+	        loop1:for(i=1000000;i>=0;i--)
+	        {
+	            if(600851475143l%i==0)         
+	            {
+	                while(j<i)
+	                {
+	                    if(i%j==0)                
+	                    break;
+	                    j++;          
+	                }
+	                if(j==i)
+                        {
+	                  System.out.println("Largest prime factor of "  +i);	
+	                  break loop1;   
+	                }
+	            }
+	            j=2;
+	        }
+	         
+
+ }          
 }
