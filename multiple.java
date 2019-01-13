9c5cae9772b7e7bc19360cd2ad86c063cf246577
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2019-01-13 21:26:08 +0530

    Update multiple.java

--- multiple.java
+++ multiple.java
@@ -3,31 +3,20 @@ package Q5;
 
 public class multiple 
 {
-    public static void main(String args[])
-    {
-        int i,j,m=0;boolean f=false;
-        for(i=1;9999999999999l;i++)
-        {
-          for(j=1;j<=20;j++) 
-          {
-              if(i%j!=0)
-              {
-                  f=true;
-                  break;
-              }             
-          }
-            if(f==false)
-            {
-                m=i;
-                break;
-            }
-            f=false;
-            
-            
-            
-        }
-        System.out.println("smallest multiple "+m);
-        
-    }    
-    
+   public static void main(String args[])
+	    {
+		 long j=2,i;boolean b=true;
+	      while(b==true)
+	       {
+	    	   long m=20;j++;
+	    	   while(m>=1)
+	    	   {
+	    		   if(b=(j%m!=0?true:false)==true)
+	    			   break;
+	    		   m--;
+	    	   }
+	       }
+	       System.out.println(j);
+	    }    
+	    
 }
