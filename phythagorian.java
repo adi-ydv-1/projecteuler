2c6d3fba363f07acfa20bddd9af559ee8e908a77
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2019-01-15 00:17:25 +0530

    Update phythagorian.java

--- phythagorian.java
+++ phythagorian.java
@@ -3,33 +3,20 @@ package Q9;
 public class phythagorian
 {
     public static void main(String args[])
-    {
-        int a,b,c,product=1,k=0;
-      loop1:for(a=1;a<1000;a++)
-        {
-            
-            for(b=a+1;b<1000;b++)
-            {
-                
-                c=(int)Math.sqrt(a*a+b*b);
-    
-                    
-                    if(a+b+c==1000&&a<b&&b<c)   
-                    {
-          
-                        if((a*a)+(b*b)==c*c)
-                        {
-                            product=a*b*c;
-                            
-                            
-                            break loop1;
-                            
-                        }   
-                    }
-                }
-                    
-              
-         }    
-               System.out.println("product of a,b,c is "+product);       
-    }
+	    {
+	        int a,b,c,product=1;
+	      loop1:for(a=1;a<1000;a++)
+	        {  
+	    	  for(b=a+1;b<1000;b++)
+	            {       
+	               c=(int)Math.sqrt(a*a+b*b); 
+	                    if(a+b+c==1000&&a<b&&b<c&&(a*a)+(b*b)==c*c)   
+	                    { 
+	                            System.out.println("product of a,b,c is "+(a*b*c));  
+	                            break loop1;     
+	                    }
+	            }    
+	         }        
+	    }
 }
+
