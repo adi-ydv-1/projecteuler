a8b6100f07c1283af24c90cdee858d17b833a001
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2019-01-18 23:36:59 +0530

    Update q_5.java

--- q_5.java
+++ q_5.java
@@ -3,25 +3,37 @@ package Q5;
 
 public class multiple 
 {
-    public static void main(String args[])
-    {
-		 int i,j;
-	       for(i=21; ;i++)
+   public static void main(String args[])
+	    {
+	       for(int i=20; ;i+=2)
 	       {
-	    	   boolean f=true;
-	    	   for( j=20;j>=1;j--)
-	            {
-	   			if(i%j!=0) 
-	   			{
-	   			f=false;
-	   			break;
-	   			}
-	            }
-	    	   if(f==true) {
+	    	   if(multiple(i)==1&&multiple_1(i)==0) {
 	    		   System.out.println(i);
-	    	   break;
+	    	       break;
 	    	   }
-	     }  
+	       }  
+	    }
+	static  int multiple(int i)
+	{
+	    int j=0;
+		for(j=19;j>=3;j-=2)
+		{
+			if(i%j!=0) {
+			break;
+			}
+		}
+		return j;
+	}
+	static  int multiple_1(int i)
+	{
+	    int j=0;
+		for(j=20;j>=2;j-=2)
+		{
+			if(i%j!=0) {
+			break;
+			}
+		}
+		return j;
 	}
 	  
 }
