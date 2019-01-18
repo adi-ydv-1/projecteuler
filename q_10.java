3aa8db135e3b44d38f046fc56ea20ca7d630dd49
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2019-01-18 22:55:58 +0530

    Update q_10.java

--- q_10.java
+++ q_10.java
@@ -4,28 +4,24 @@ public class sumprime
 {
     public static void main(String args[])
     {
-        long i,sum=0,k=2l;boolean f=false;
-        for(i=2;i<2000000L;i++)
-        {
-            while(k<i)
-            {
-                if(i%k==0)
-                {
-                    f=true;
-                    break;
-                }
-                k++;
-                
-                
-            }
-            if(f==false)
-            {
-                  
-                sum=sum+i;  
-            }
-            k=2;
-            f=false;    
-        }
-        System.out.println("prime no below two million is "+sum);    
+       long i,sum=2;
+       for(i=3;i<2000000L;i+=2)
+       {
+    	   if(prime(i)==(int)(Math.sqrt(i)+1))
+    		   sum+=i; 
+       }
+    	System.out.println(sum);	
     }
-}
+    public static long prime (long n)
+    {
+    	long i;
+    	for( i=2;i<=(int)Math.sqrt(n);i++)
+    	{
+    		if(n%i==0) 
+    			break; 
+    	}
+    	return i ;
+    	
+    }
+    
+} 
