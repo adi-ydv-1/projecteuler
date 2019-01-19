82f08152fd0fb6e384afc6508c477f67072ce2d9
yadav5113 <45618714+yadav5113@users.noreply.github.com>
2019-01-19 22:24:25 +0530

    Update q_10.java

--- q_10.java
+++ q_10.java
@@ -2,26 +2,32 @@
 package Q10
 public class sumprime
 {
-    public static void main(String args[])
+   public static void main(String args[])
     {
        long i,sum=2;
        for(i=3;i<2000000L;i+=2)
        {
-    	   if(prime(i)==(int)(Math.sqrt(i)+1))
+    	   if(prime(i)==false)
     		   sum+=i; 
        }
-    	System.out.println(sum);	
+    	System.out.println(sum);
+    	
+    	
     }
-    public static long prime (long n)
+    public static boolean prime (long n)
     {
-    	long i;
+    	long i;boolean f=false;
     	for( i=2;i<=(int)Math.sqrt(n);i++)
     	{
     		if(n%i==0) 
+    		{
+    			f=true;
     			break; 
+    			
+    		}
     	}
-    	return i ;
+    	return f ;
     	
     }
     
-} 
+}
